ef929f8265ec008464fb11f6155f2457b7836c5f
Vincent-Nguyen2005 <nguyenvanhuy.technologist@gmail.com>
2026-03-26 13:41:52 +0700

    Create test_login.php

---
+++ test_login.php
@@ -0,0 +1,5 @@
+<form method="POST" action="backend/auth.php">
+    Username: <input type="text" name="username"><br>
+    Password: <input type="text" name="password"><br>
+    <button type="submit">Login</button>
+</form>
